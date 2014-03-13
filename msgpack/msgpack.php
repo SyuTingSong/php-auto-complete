1baef15f2f96b6cb76914949c779e739d622f67e
TingSong-Syu <tingsongx@oupeng.com>
2014-03-13 14:35:59 +0800

    add autocomplete for swoole & msgpack

---
+++ msgpack/msgpack.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @param mixed $data
+ * @return string
+ */
+function msgpack_pack($data) {}
+
+/**
+ * @param string $pack
+ * @return mixed
+ */
+function msgpack_unpack($pack) {}
+
