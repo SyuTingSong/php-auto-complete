65adff7f1aa20bdbbed774b1c92795461600eeae
TingSong-Syu <tingsongx@oupeng.com>
2014-11-12 15:01:23 +0800

    update

---
+++ xhprof/xhprof.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * orz
+ * File: xhprof.php
+ * User: TingSong-Syu <tingsongx@oupeng.com>
+ * Date: 2014-03-20
+ * Time: 11:04
+ */
+
+const XHPROF_FLAGS_NO_BUILTINS = 1;
+const XHPROF_FLAGS_CPU = 2;
+const XHPROF_FLAGS_MEMORY = 4;
+
+/**
+ * @param int $flags
+ * @param array $options
+ */
+function xhprof_enable($flags=0, $options=array()) {}
+
+/**
+ * @return array
+ */
+function xhprof_disable() {}
+
---
+++ zmq/ZMQDeviceException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * orz
+ * File: ZMQDeviceException.php
+ * User: TingSong-Syu <tingsongx@oupeng.com>
+ * Date: 2014-03-17
+ * Time: 11:58
+ */
+
+class ZMQDeviceException extends Exception {}
